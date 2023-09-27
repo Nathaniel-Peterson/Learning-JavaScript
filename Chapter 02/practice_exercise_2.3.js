1cff4f1e6a49b972b18ed1a1af060ef2defada02
Nathaniel-Peterson <135255679+Nathaniel-Peterson@users.noreply.github.com>
2023-09-27 17:51:12 -0500

    Changes to 2.3
    Added comments to the program.

--- Chapter 02/practice_exercise_2.3.js
+++ Chapter 02/practice_exercise_2.3.js
@@ -1,5 +1,5 @@
 var a = prompt("Enter value for the hight of the triangle: ");
 var b = prompt("Enter value for the base of the triangle: ");
-var c = (a * a + b * b);
+var c = (a * a + b * b); // Pythagorean theorem formula
 
 console.log("The hypotenuse of the triangle is " + c + ".");
