3b82e5d15472e763b467d0b351ff240909fe7413
Nathaniel-Peterson <135255679+Nathaniel-Peterson@users.noreply.github.com>
2023-10-17 17:25:13 -0500

    Chapter 2 Project
    Chapter 2 Project was a miles to kilometers converter.

---
+++ Chapter 02/chapter02_project.js
@@ -0,0 +1,5 @@
+//Miles-to-kilometers Converter
+//1 mile equals 1.60934 kilometers
+let myDistanceM = 146;
+let myDistanceKM = myDistanceM * 1.60934;
+console.log("The distance of " + myDistanceM + " miles is equal to " + myDistanceKM + " kms.")
